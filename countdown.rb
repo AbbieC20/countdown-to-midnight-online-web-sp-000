26c6dd1983d7d953666bc7a8782389b9f8908054
remote-torrent-6747 <abbyc@sky.com>
2020-12-05 15:31:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(time)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(time)
+  time = 10
+  while time > 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
