5ca5c81f253cd2e174550ec61d4d15a5b0fd997e
remote-torrent-6747 <abbyc@sky.com>
2020-12-05 15:19:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,12 +5,3 @@ def countdown(time)
   end
   puts "HAPPY NEW YEARS!"
 end
-
-def countdown_with_sleep
-end
-
-
-Write a method that takes in an integer argument and uses a while loop to countdown from that
-integer to 1, outputting "#{number} SECOND(S)!" in each iteration of the loop. The method
-should return "HAPPY NEW YEAR!" after the loop finishes.
-Hint: In Ruby, a method will return the very last line of code that it executes.
